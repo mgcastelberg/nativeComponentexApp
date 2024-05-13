f662b81f729d52e68b47b7e14ff772d2eb9753de
Manuel Gomez <jmgc@virket.com>
2024-05-13 08:35:04 -0600

    Preparación y estructura del proyecto

---
+++ src/ComponentsApp.tsx
@@ -0,0 +1,11 @@
+
+import { Text, View } from 'react-native'
+import { HomeScreen } from './presentation/screens/home/HomeScreen'
+
+export const ComponentsApp = () => {
+    return (
+        <View>
+            <HomeScreen/>
+        </View>
+    )
+}
