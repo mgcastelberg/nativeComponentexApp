d0a196dfa8d656238dcccc375b72eecaf57f4bf1
Manuel Gomez <jmgc@virket.com>
2024-05-14 19:45:43 -0600

    Opciones del menú

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -3,22 +3,24 @@ import { Text, View } from 'react-native'
 import { globalStyles } from '../../../config/theme/theme';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Title } from '../../components/ui/Title';
+import { MenuItem } from '../../components/ui/MenuItem';
 // import { AirplaneIcon } from '../../icons/Icons'
 
-export const menuItems = [
+const animationMenuItems = [
     // 01-animationMenuItems
     {
-      name: 'Animation 101',
-      icon: 'cube-outline',
-      component: 'Animation101Screen',
+        name: 'Animation 101',
+        icon: 'cube-outline',
+        component: 'Animation101Screen',
     },
     {
-      name: 'Animation 102',
-      icon: 'albums-outline',
-      component: 'Animation102Screen',
+        name: 'Animation 102',
+        icon: 'albums-outline',
+        component: 'Animation102Screen',
     },
-  
-  
+]
+
+export const menuItems = [
     // 02-menuItems
     {
       name: 'Pull to refresh',
@@ -50,23 +52,25 @@ export const menuItems = [
       icon: 'flask-outline',
       component: 'ChangeThemeScreen',
     },
+  ];
   
+  const uiMenuItems = [
     // 03- uiMenuItems
     {
-      name: 'Switches',
-      icon: 'toggle-outline',
-      component: 'SwitchScreen',
-    },
-    {
-      name: 'Alerts',
-      icon: 'alert-circle-outline',
-      component: 'AlertScreen',
-    },
-    {
-      name: 'TextInputs',
-      icon: 'document-text-outline',
-      component: 'TextInputScreen',
-    },
+        name: 'Switches',
+        icon: 'toggle-outline',
+        component: 'SwitchScreen',
+      },
+      {
+        name: 'Alerts',
+        icon: 'alert-circle-outline',
+        component: 'AlertScreen',
+      },
+      {
+        name: 'TextInputs',
+        icon: 'document-text-outline',
+        component: 'TextInputScreen',
+      },
   ];
 
 export const HomeScreen = () => {
@@ -76,8 +80,20 @@ export const HomeScreen = () => {
                 <ScrollView>
                     <Title text='Opciones del menú' safe />
                     {
-                        menuItems.map( item => (
-                            <Text key={item.component}>{item.name}</Text>
+                        animationMenuItems.map( (item,index) => (
+                            <MenuItem key={item.component} {...item} isFirst={ index === 0} isLast={ index === animationMenuItems.length -1}/>
+                        ))
+                    }
+                    <View style={{ marginTop:30}}/>
+                    {
+                        menuItems.map( (item,index) => (
+                            <MenuItem key={item.component} {...item} isFirst={ index === 0} isLast={ index === menuItems.length -1}/>
+                        ))
+                    }
+                    <View style={{ marginTop:30}}/>
+                    {
+                        uiMenuItems.map( (item,index) => (
+                            <MenuItem key={item.component} {...item} isFirst={ index === 0} isLast={ index === uiMenuItems.length -1}/>
                         ))
                     }
                 </ScrollView>
